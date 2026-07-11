cc18fec67d26e3648eac3e587575729b87129918
Nidhi Patil <nidhipatil203@gmail.com>
2026-07-11 17:05:05 +0530

    ✅ [Easy] #1899 - Count Items Matching a Rule | Java | Runtime: 7 ms | Memory: 49.3 MB

--- Easy/1899-count-items-matching-a-rule/Solution.java
+++ Easy/1899-count-items-matching-a-rule/Solution.java
@@ -1,19 +1,17 @@
 class Solution {
     public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
-        int index=0;
-        if(ruleKey.equals("color")){
-            index=1;
-        }
-        else if(ruleKey.equals("name")){
-            index=2;
-        }
-        int c=0;
+        int count=0;
         for(int i=0; i<items.size(); i++){
-
-            if(items.get(i).get(index).equals(ruleValue)){
-                c++;
+            if(ruleKey.equals("type") && items.get(i).get(0).equals(ruleValue)){
+                count++;
+            }
+            if(ruleKey.equals("color") && items.get(i).get(1).equals(ruleValue)){
+                count++;
+            }
+            if(ruleKey.equals("name") && items.get(i).get(2).equals(ruleValue)){
+                count++;
             }
         }
-        return c;
+        return count;
     }
 }
