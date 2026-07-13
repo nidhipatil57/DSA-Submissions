2ebc7ef5fa69d6680f4759db8d42a07847438a45
Nidhi Patil <nidhipatil203@gmail.com>
2026-07-14 02:00:39 +0530

    ✅ [Easy] #0599 - Minimum Index Sum of Two Lists | Java | Runtime: 113 ms | Memory: 47.6 MB

---
+++ Easy/0599-minimum-index-sum-of-two-lists/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String[] findRestaurant(String[] list1, String[] list2) {
+        ArrayList<String> ans = new ArrayList<>();
+        int min = Integer.MAX_VALUE;
+        for(int i=0;i<list1.length;i++){
+            for(int j=0;j<list2.length;j++){
+                if(list1[i].equals(list2[j])){
+                    if(i+j<min){
+                        ans.clear();
+                        ans.add(list1[i]);
+                        min=i+j;
+                    }
+                    else if(i+j==min){
+                        ans.add(list1[i]);
+                    }
+                }
+            }
+        }
+        return ans.toArray(new String[0]);
+    }
+}
