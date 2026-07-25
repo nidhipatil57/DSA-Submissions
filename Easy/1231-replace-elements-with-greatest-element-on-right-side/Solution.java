029d9e8385980d99b1a53a141eda24334a7432cd
Nidhi Patil <nidhipatil203@gmail.com>
2026-07-25 07:41:33 +0530

    ✅ [Easy] #1231 - Replace Elements with Greatest Element on Right Side | Java | Runtime: 3 ms | Memory: 48.8 MB

--- Easy/1231-replace-elements-with-greatest-element-on-right-side/Solution.java
+++ Easy/1231-replace-elements-with-greatest-element-on-right-side/Solution.java
@@ -1,14 +1,11 @@
 class Solution {
     public int[] replaceElements(int[] arr) {
-        int[] ans=new int[arr.length];
-        for(int i=0; i<arr.length-1; i++){
-            int max=Integer.MIN_VALUE;
-            for(int j=i+1; j<arr.length; j++){
-                max=Math.max(max, arr[j]);
-            }
-            ans[i]=max;
+        int max=-1;
+        for(int i=arr.length-1; i>=0; i--){
+            int temp=arr[i];
+            arr[i]=max;
+            max=Math.max(max, temp);
         }
-        ans[arr.length-1]=-1;
-        return ans;
+        return arr;
     }
 }
