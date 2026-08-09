2850441d986ff4ba5021399257f7f6488250ba28
Nidhi Patil <nidhipatil203@gmail.com>
2026-08-09 12:58:55 +0530

    ✅ [Medium] #0909 - Stone Game | Java | Runtime: 0 ms | Memory: 43.4 MB

--- Medium/0909-stone-game/Solution.java
+++ Medium/0909-stone-game/Solution.java
@@ -1,17 +1,9 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        Arrays.sort(piles);
-        int alice=0;
-        int bob=0;
-        for(int i=piles.length-1; i>=0; i-=2){
-            alice=alice+piles[i];
-        }
-        for(int i=piles.length-2; i>=0; i-=2){
-            bob=bob+piles[i];
-        }
-        if(alice>bob){
         return true;
-        }
-        return false;
+        /*There are an even number of piles, so Alice can always
+        choose a strategy to take either all odd-positioned or
+        all even-positioned piles, whichever has the larger sum.
+        Therefore Alice can always win.*/
     }
 }
