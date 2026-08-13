dd2e52d18e73c72fd36838e06cb2419662e81daa
Nidhi Patil <nidhipatil203@gmail.com>
2026-08-13 20:47:41 +0530

    ✅ [Easy] #0890 - Lemonade Change | Java | Runtime: 2 ms | Memory: 72.3 MB

---
+++ Easy/0890-lemonade-change/Solution.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five=0;
+        int ten=0;
+        for(int i=0; i<bills.length; i++){
+            if(bills[i]==5){
+                five++;
+            }
+            else if(bills[i]==10){
+                if(five<1){
+                    return false;
+                }
+                ten++;
+                five--;
+            }
+            else{
+                if(bills[i]==20 && ten>=1 && five>=1){
+                    ten--;
+                    five--;
+                }
+                else if(bills[i]==20 && five>=3){
+                    five=five-3;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
