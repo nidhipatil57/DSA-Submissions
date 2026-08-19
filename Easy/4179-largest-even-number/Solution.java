4fb9beba579132cf41523209580180ce521e019b
Nidhi Patil <nidhipatil203@gmail.com>
2026-08-20 00:10:14 +0530

    ✅ [Easy] #4179 - Largest Even Number | Java | Runtime: 1 ms | Memory: 43.9 MB

---
+++ Easy/4179-largest-even-number/Solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String largestEven(String s) {
+        int last=s.lastIndexOf('2');
+        if(last==-1) {
+            return "";
+        }
+        return s.substring(0, last + 1);
+    }
+}
