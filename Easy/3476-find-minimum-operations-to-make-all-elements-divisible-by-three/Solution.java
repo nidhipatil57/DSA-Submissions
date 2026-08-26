928899ca3891560013526f90869a35d6522715fa
Nidhi Patil <nidhipatil203@gmail.com>
2026-08-27 00:25:52 +0530

    ✅ [Easy] #3476 - Find Minimum Operations to Make All Elements Divisible by Three | Java | Runtime: 1 ms | Memory: 43.8 MB

---
+++ Easy/3476-find-minimum-operations-to-make-all-elements-divisible-by-three/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int minimumOperations(int[] nums) {
+        int c=0;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]%3==0){
+                c=c+0;
+            }
+            if((nums[i]-1)%3==0 || (nums[i]+1)%3==0){
+                c++;
+            }
+        }
+        return c;
+    }
+}
